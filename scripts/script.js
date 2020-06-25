f763f4fa39b779f221b352ad42c9268273ef6708
jesusdlaz <67393362+jesusdlaz@users.noreply.github.com>
2020-06-25 12:52:07 -0500

    ok

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+display: inherit;
+/* En ves de una posicion horizontal sera vertical > de 600 */
+height: auto;
+/* Ya no tenga una altura predeterminada  */
+
+//Se linkea a el html 
+//Por debajo de los 600 pixeles active todas las reglas de esa seccion
